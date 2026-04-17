83d6ce8788a76b28a0ca32f10c8b9487c5a86a6c
sunny-zerror <sunny.zerror@gmail.com>
2026-04-17 13:40:58 +0530

    facebook meta added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-scroll-behavior="smooth">
-      <Head />
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="bq9nj6cv94r3246gb16h08zac7d0vb"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
