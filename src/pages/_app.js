a949df3c8b190f20a488d4f66c8a8dd9f9ccd8b8
sunny-zerror <sunny.zerror@gmail.com>
2026-04-20 12:40:42 +0530

    facebook pixel added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -50,8 +50,40 @@ export default function App({ Component, pageProps }) {
     }
   }, []);
 
+   useEffect(() => {
+    const handleRouteChange = () => {
+      window.fbq && window.fbq("track", "PageView");
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '756441694101905');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
       {/* Google Ads / Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-17387356943"
